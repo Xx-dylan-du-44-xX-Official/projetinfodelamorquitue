09dc9350fc1faa7226fe848dabe3ff43a8cdea7d
Dylan-du-44 <45388895+Xx-dylan-du-44-xX-Official@users.noreply.github.com>
2018-12-04 10:14:16 +0100

    Update python info projet.py

--- python info projet.py
+++ python info projet.py
@@ -89,3 +89,82 @@ def jeu(joueurs):
                 joueurs = acheter(joueurs[2][numero_joueur], numero_joueur)
                 joueurs = payer(joueurs[2][numero_joueur], numero_joueur)
     return joueurs
+
+############################## xd
+from math import *
+from random import randint, random
+
+##
+'''
+joueur 1 achète petites cases
+joueur 2 achete grosses cases
+joueur 3 aléatoire
+joueur 4 achete rien
+J1 = 0
+J2 = 0
+Cases(J1) = [[],[]]
+Cases[J1]
+'''
+##
+L = [2,-1,-2,0,-3,-4]
+
+position_j1 = position_j2 = 0
+argent_j1 = argent_j2 = 5
+
+Cases_j1 = [2,1,2,0,3,0]
+Cases_j2 = [2,0,0,0,0,4]
+Cases_j3 = [2]
+for k in range(1,len(L)-1):
+    if random() <= 1/2 :
+        Cases_j3.append(L[k])
+    else : Cases_j3.append(0)
+
+joueurs = [[argent_j1,argent_j2],[Cases_j1, Cases_j2],[position_j1,position_j2]]
+nb_joueurs = len(joueurs[0])
+
+def lancerdede():
+    return randint(1,6)
+
+def acheter(position, numero_joueur):
+    '''
+    entrée : position du joueur, numero du joueur
+    sortie : informations des joueurs si le joueur avec lequel on travaille achète une propriété 
+    '''
+    if joueurs[0][numero_joueur] > joueurs[1][numero_joueur][position] and position != 0 and joueurs[1][numero_joueur][position] != 0:
+        joueurs[0][numero_joueur] -= joueurs[1][numero_joueur][position]
+        for i in range(nb_joueurs) :
+            if i != numero_joueur :
+                joueurs[1][i][position] -= joueurs[1][numero_joueur][position]
+        joueurs[1][numero_joueur][position] = 0
+    return joueurs
+
+def payer(position, numero_joueur):
+    '''
+    entrée : position du joueur, numero du joueur
+    sortie : informations des joueurs si le joueur avec lequel on travaille tombe sur une case achetée par un autre joueur
+    '''
+    if joueurs[1][numero_joueur][position] < 0:
+        joueurs[0][numero_joueur] += joueurs[1][numero_joueur][position]
+    for k in range(nb_joueurs):
+        if k != numero_joueur:
+            if joueurs[1][k][position] == 0 :
+                joueurs[0][k] -= joueurs[1][numero_joueur][position]
+    return joueurs
+
+def jeu(joueurs):
+    position = 0
+    numero_joueur = 0
+    #numero_joueur = randint(0,nb_joueurs)
+    for k in range(nb_joueurs):
+        while joueurs[0][k] > 0:
+            r = lancerdede()
+            joueurs[2][numero_joueur] += r
+            if joueurs[2][numero_joueur] > len(L) : 
+                joueurs[2][numero_joueur] -= len(L)
+                joueurs[0][numero_joueur] += 2
+            joueurs = acheter(joueurs[2][numero_joueur], numero_joueur)
+            joueurs = payer(joueurs[2][numero_joueur], numero_joueur)
+            numero_joueur += 1
+            if numero_joueur >= nb_joueurs : 
+                numero_joueur -= nb_joueurs
+    return joueurs
